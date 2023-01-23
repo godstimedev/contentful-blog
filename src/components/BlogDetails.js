744ebe5b067b16cbf364df066f1a16daa7867e41
Godstime Agholor <agholorgodstime18@gmail.com>
2023-01-23 19:07:02 +0100

    finish

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import { createClient } from 'contentful';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const BlogDetails = () => {
-	return <div>BlogDetails</div>;
+	const [singlePost, setSinglePost] = useState([]);
+
+	const client = createClient({
+		space: 'i9sgbsr5ydfe',
+		accessToken: 'XZhFiFOZIZ_bqzOYECxKF740VSiF3VLBAoWQoULbRSs',
+	});
+
+	const { id } = useParams();
+	console.log(id);
+
+	useEffect(() => {
+		const getEntriesById = async () => {
+			try {
+				await client.getEntry(id).then((entry) => {
+					console.log(entry);
+					setSinglePost(entry);
+				});
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getEntriesById();
+	}, []);
+
+	return (
+		<section className="p-4 md:px-[4rem]">
+			<h1 className="text-4xl font-bold my-6 text-center">Blog Post</h1>
+			<article className="items-center flex flex-col gap-2">
+				<img
+					className="h-[250px]"
+					src={singlePost?.fields?.blogImage?.fields?.file?.url}
+					alt={singlePost?.fields?.blogImage?.fields?.title}
+				/>
+				<h2 className="font-semibold text-xl">{singlePost?.fields?.blogTitle}</h2>
+				<div className="w-full flex justify-between">
+					<p className="text-xl font-semibold">{singlePost?.fields?.blogAuthor}</p>
+					<small className="text-[#333] font-semibold text-md">{singlePost?.fields?.createdDate}</small>
+				</div>
+				<p>{singlePost?.fields?.postContent}</p>
+			</article>
+		</section>
+	);
 };
 
 export default BlogDetails;
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,59 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { createClient } from 'contentful';
+import { Link } from 'react-router-dom';
 
 const BlogList = () => {
+	const [blogPost, setBlogPost] = useState([]);
+
+	const client = createClient({
+		space: 'i9sgbsr5ydfe',
+		accessToken: 'XZhFiFOZIZ_bqzOYECxKF740VSiF3VLBAoWQoULbRSs',
+	});
+
+	useEffect(() => {
+		const getAllEntries = async () => {
+			try {
+				await client.getEntries().then((entries) => {
+					console.log(entries.items);
+					setBlogPost(entries.items);
+				});
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getAllEntries();
+	}, [client]);
+
 	return (
 		<section className="p-4 ">
 			<h1 className="text-4xl font-bold my-6 text-center">Blog List</h1>
-			<div className="grid grid-cols-2 gap-6 place-items-center ">
-				<article className=" max-w-[400px] flex flex-col gap-2">
-					<img
-						className=""
-						src="https://plus.unsplash.com/premium_photo-1663047812698-fdd05e787f00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-						alt="/"
-					/>
-					<h2 className="font-semibold text-xl">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, optio.
-					</h2>
-					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil voluptatum, quia ipsum
-						corporis quidem officiis in molestias, sequi rem suscipit minima temporibus explicabo
-						distinctio nemo ipsa, voluptate aliquid iste sapiente
-					</p>
-					<small className="text-[#333] font-semibold text-md">20, Jan 2022</small>
-				</article>
-				<article className=" max-w-[400px] flex flex-col gap-2">
-					<img
-						className=""
-						src="https://plus.unsplash.com/premium_photo-1661905415350-a3e95a0f8f74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-						alt="/"
-					/>
-					<h2 className="font-semibold text-xl">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, optio.
-					</h2>
-					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil voluptatum, quia ipsum
-						corporis quidem officiis in molestias, sequi rem suscipit minima temporibus explicabo
-						distinctio nemo ipsa, voluptate aliquid iste sapiente
-					</p>
-					<small className="text-[#333] font-semibold text-md">20, Jan 2022</small>
-				</article>
-				<article className=" max-w-[400px] flex flex-col gap-2">
-					<img
-						className=""
-						src="https://images.unsplash.com/photo-1661961110372-8a7682543120?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-						alt="/"
-					/>
-					<h2 className="font-semibold text-xl">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, optio.
-					</h2>
-					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil voluptatum, quia ipsum
-						corporis quidem officiis in molestias, sequi rem suscipit minima temporibus explicabo
-						distinctio nemo ipsa, voluptate aliquid iste sapiente
-					</p>
-					<small className="text-[#333] font-semibold text-md">20, Jan 2022</small>
-				</article>
-			</div>
+			{blogPost && (
+				<div className="grid grid-cols-2 gap-6 place-items-center ">
+					{blogPost?.map((post) => (
+						<article key={post.sys.id} className=" max-w-[400px] flex flex-col gap-2">
+							<img
+								className="h-[250px]"
+								src={post.fields.blogImage.fields.file.url}
+								alt={post.fields.blogImage.fields.title}
+							/>
+							<Link to={`/blogDetails/${post.sys.id}`}>
+								<h2 className="font-semibold text-xl">{post.fields.blogTitle}</h2>
+								<p>{post.fields.blogSummary}</p>
+								<small className="text-[#333] font-semibold text-md">{post.fields.createdDate}</small>
+							</Link>
+						</article>
+					))}
+				</div>
+			)}
 		</section>
 	);
 };
